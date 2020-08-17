240bbd4a618e2c33ed05596d14f6f64e39a6ea95
kh-jyp <rjqnrwodus123@gmail.com>
2020-08-17 10:07:25 +0900

    asdf

---
+++ HelloGit/src/main/java/com/kh/hellogit/zzim/controller/ZzimController.java
@@ -0,0 +1,10 @@
+package com.kh.hellogit.zzim.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ZzimController {
+	@RequestMapping("login.do")
+	public String ZzimLogin(String id, String pwd) {
+		return "home";
+	}
+}
